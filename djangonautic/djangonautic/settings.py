83fdd5151fc9af1efddb1a52514c856ecadefa51
manay singi <manaysingi98@gmail.com>
2018-09-16 19:31:42 +0530

    hffh

--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -148,3 +148,7 @@ if os.getcwd() == '/app':
 	)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR,'media')
