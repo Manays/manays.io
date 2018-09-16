0146a0e39afd6cecf4f0f27c09eec101e1bffaaa
manay singi <manaysingi98@gmail.com>
2018-09-16 19:23:41 +0530

    dd

--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -142,9 +142,9 @@ if os.getcwd() == '/app':
 	ALLOWED_HOSTS = ['write-blogs.herokuapp.com']
 
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 	STATIC_URL = '/static/'
 	STATICFILES_DIRS = (
 		os.path.join(BASE_DIR,'static'),
 	)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
