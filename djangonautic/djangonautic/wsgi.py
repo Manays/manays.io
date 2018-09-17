3878f24a2300db61adf9037f65e582479ca96fe3
manay singi <manaysingi98@gmail.com>
2018-09-17 12:18:15 +0530

    ss

--- djangonautic/djangonautic/wsgi.py
+++ djangonautic/djangonautic/wsgi.py
@@ -7,11 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangonautic.settings')
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
